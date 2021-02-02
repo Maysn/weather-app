222cb0d03960609dfc9993311857965fe550dd22
Maysn <mmazen1321@gmail.com>
2021-02-02 22:29:48 +0200

    first-iteration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [location, setLocation] = useState({});
+    const [weather, setWeather] = useState();
+
+    useEffect(() => {
+      fetch("http://ip-api.com/json/")
+        .then(y => y.json())
+        .then(data => setLocation(data));
+    },[]);
+
+    useEffect(() => { async function getWeather(city){
+      if (location && location.city) {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1c2d09630b095c17f9c51ac1302665f9`)
+      const responseJson = await response.json();
+      setWeather(responseJson);
+      }
+    }
+        getWeather(location.city);
+    },[location])
+    console.log(weather);
+    return (
+      <div>
+        <h1>{weather? `${Math.round(weather.main.temp - 273.15)}°` : "Loading..."}</h1>
+        <h2>{weather? `IT'S ${weather.weather[0].main.toUpperCase()}.. STAY HOME!`: "Loading..."}</h2>
+        <h3>{weather? weather.name: "Loading..."}</h3>
+      </div>
   );
 }
 
