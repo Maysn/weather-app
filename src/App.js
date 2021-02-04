59b94bcc917184ff91fa6de322f4204369d9a1d6
Maysn <mmazen1321@gmail.com>
2021-02-04 02:00:57 +0200

    weather-appv1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,72 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-    const [location, setLocation] = useState({});
-    const [weather, setWeather] = useState();
+  const [location, setLocation] = useState({});
+  const [weather, setWeather] = useState();
 
-    useEffect(() => {
-      const myKey = 'df35fbdda85415498def473162c912f8b4edac30e599f6d0eb4e9025';
-      fetch(`https://api.ipdata.co?api-key=${myKey}`)
-        .then(y => y.json())
-        .then(data => setLocation(data));
-    },[]);
+  useEffect(() => {
+    // const myKey = 'df35fbdda85415498def473162c912f8b4edac30e599f6d0eb4e9025';
+    // fetch(`https://api.ipdata.co?api-key=${myKey}`)
+    fetch("http://ip-api.com/json")
+      .then((y) => y.json())
+      .then((data) => setLocation(data));
+  }, []);
 
-    useEffect(() => { async function getWeather(city){
-      if (location && location.city) {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1c2d09630b095c17f9c51ac1302665f9`)
+  useEffect(() => {
+    async function getWeather(city) {
+      if (location.city) {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1c2d09630b095c17f9c51ac1302665f9`
+        );
         const responseJson = await response.json();
         setWeather(responseJson);
       }
     }
-        getWeather(location.city);
-     },[location])
-    console.log(weather);
+    getWeather(location.city);
+  }, [location]);
+  console.log(weather);
 
-    const weatherDesc= weather? weather.weather[0].main : '';
-    
-    return (
-      <div className={weatherDesc.toLowerCase() === 'clear' ? 'container clear' : 
-                      weatherDesc.toLowerCase() === 'clouds' ? 'container cloudy' : 
-                      weatherDesc.toLowerCase() === 'rain' ? 'container rainy' : 
-                      weatherDesc.toLowerCase() === 'snow' ? 'container snow' : ''}>
-        <div>
-         <div>
-            <h1>{weather? weather.name: "Loading..."}</h1>
-            <h1>{weather? `${Math.round(weather.main.temp - 273.15)}°` : "Loading..."}</h1>
-         </div>
-         <div>
-            <h1>{weather? `${weatherDesc.toUpperCase()}.. STAY HOME!`: "Loading..."}</h1>
-         </div>
+  const weatherDesc = weather ? weather.weather[0].main.toLowerCase() : "";
+
+  return (
+    <div
+      className={
+        weatherDesc === "clear"
+          ? "container clear"
+          : weatherDesc === "clouds"
+          ? "container cloudy"
+          : weatherDesc === "rain"
+          ? "container rainy"
+          : weatherDesc === "snow"
+          ? "container snow"
+          : weatherDesc === "haze"
+          ? "container haze"
+          : ""
+      }
+    >
+      {weather ? (
+        <div className="grid">
+            <h1 className='forecast'>{`${Math.round(weather.main.temp - 273.15)}°`}</h1>
+            <h2 className='forecast'>{weather.name}</h2>
+            <h3 className='forecast'>
+              {weatherDesc === "clouds"
+                ? "It's sooo cloudy.. STAY HOME!"
+                : weatherDesc === "rain"
+                ? "It's sooo rainy.. STAY HOME!"
+                : weatherDesc === "snow"
+                ? "It's sooo snowie.. STAY HOME!"
+                : weatherDesc === "clear"
+                ? "Sky is so clear.. STAY HOME!"
+                : weatherDesc === "haze"
+                ? "It's sooo haze.. STAY HOME!"
+                : ""}
+            </h3>
         </div>
-      </div>
-        
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
 
